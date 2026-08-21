01f38ff6373c86f67f6e625130542184558246ec
Sohan <thamminenisohan1977@gmail.com>
2026-08-21 19:06:54 +0530

    Time: 55 ms (9.76%), Space: 11.9 MB (64.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -1,6 +1,6 @@
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int size = nums1Size+nums2Size;
-    double combo[size];
+    
+    double combo[nums1Size+nums2Size];
     
     
     
@@ -10,8 +10,8 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
     for(int i = 0;i<nums2Size;i++){
         combo[(nums1Size)+i] = nums2[i];
     }
-    for (int i = 0;i<size;i++){
-        for (int j = i;j<size;j++){
+    for (int i = 0;i<(nums1Size+nums2Size);i++){
+        for (int j = i;j<(nums1Size+nums2Size);j++){
             if(combo[i]>combo[j]){
                 int temp;
                 temp = combo[i];
@@ -20,11 +20,11 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
             }
         }
     }
-    if(size%2==1){
-        return combo[size/2];
+    if((nums1Size+nums2Size)%2==1){
+        return combo[(nums1Size+nums2Size)/2];
     }
     else{
-        double x = (combo[size/2] + combo[(size/2)-1])/2;
-        return x;
+        return (combo[(nums1Size+nums2Size)/2] + combo[((nums1Size+nums2Size)/2)-1])/2;
+        
     }
 }
