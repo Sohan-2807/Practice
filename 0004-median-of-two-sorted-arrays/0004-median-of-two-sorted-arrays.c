154aa3966acff0fb4624e339d72fe68a31185c5d
Sohan <thamminenisohan1977@gmail.com>
2026-08-21 19:02:33 +0530

    Time: 60 ms (9.06%), Space: 11.9 MB (64.59%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -0,0 +1,29 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int size = nums1Size+nums2Size;
+    double combo[size];
+    int temp;
+    
+    
+    for (int i = 0;i<nums1Size;i++){
+        combo[i] = nums1[i];
+    }
+    for(int i = 0;i<nums2Size;i++){
+        combo[(nums1Size)+i] = nums2[i];
+    }
+    for (int i = 0;i<size;i++){
+        for (int j = i;j<size;j++){
+            if(combo[i]>combo[j]){
+                temp = combo[i];
+                combo[i] = combo[j];
+                combo[j] = temp;
+            }
+        }
+    }
+    if(size%2==1){
+        return combo[size/2];
+    }
+    else{
+        double x = (combo[size/2] + combo[(size/2)-1])/2;
+        return x;
+    }
+}
