f2fbb9eff78d30b619cb8b94092637565ac93f4d
Sohan <thamminenisohan1977@gmail.com>
2026-08-24 22:14:18 +0530

    Time: 1058 ms (6.82%), Space: 8.5 MB (59.89%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.c
@@ -0,0 +1,13 @@
+bool checkPerfectNumber(int num) {
+    int sum = 0;
+    for(int i = 1;i<=num/2;i++){
+        if (num%i == 0){
+            sum += i;
+        }
+    }
+    if(num==sum){
+        return true;
+    }
+    return false;
+
+}
