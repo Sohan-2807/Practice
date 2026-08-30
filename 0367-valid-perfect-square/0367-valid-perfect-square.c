d3f7f61a1994ac811e9049266ceed8266235a649
Sohan <thamminenisohan1977@gmail.com>
2026-08-30 12:24:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (17.2%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,10 +1,12 @@
 bool isPerfectSquare(int num) {
-    long long int i = 1;
-    while(i<= num){
-        if(i*i == num){
+    long long i = 1;
+
+    while (i * i <= num) {
+        if (i * i == num) {
             return true;
         }
         i++;
     }
+
     return false;
 }
