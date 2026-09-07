985e437e91a6bf11d815c79bf7f5c1e93bc11a1e
Sohan <thamminenisohan1977@gmail.com>
2026-09-07 23:13:20 +0530

    Time: 0 ms (100%), Space: 15.2 MB (28.44%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -0,0 +1,16 @@
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize * 2;
+
+    int* ans = malloc((*returnSize) * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        ans[i] = nums[i];
+        ans[i + numsSize] = nums[i];
+    }
+
+    return ans;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
