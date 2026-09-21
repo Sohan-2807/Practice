be5b9ee1cc0187c676bfa797efd4940850125294
Sohan <thamminenisohan1977@gmail.com>
2026-09-21 23:57:05 +0530

    Time: 0 ms (100%), Space: 12.9 MB (75.5%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,16 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int k = 1;
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
